76ede2784fa837aa454e7a08a03cf9a51305bcfe
Kyle Ng <kgng@usc.edu>
2025-08-31 16:57:10 -0700

    wip tokenizer class

---
+++ src/tokenization/tokenizer.py
@@ -0,0 +1,22 @@
+import re
+import string
+
+CJK_RANGE = r"\u4E00-\u9FFF"
+SEGMENT = re.compile(fr"[{CJK_RANGE}]+|[^\s{CJK_RANGE}{string.punctuation}]+|[{string.punctuation}]+")
+SPACE = re.compile(r"\s+")
+
+# Helper Functions for Sennrich et al.
+
+def segment_words(text:str) -> list[str]:
+    """
+    For whitespace delimited languages: Converts a sentence/continuous text into words
+    i.e. "This sentence is short" => ["this", "sentence", "is", "short"]
+    
+    For non-whitespce delimited languages: Converts a sentence/continuous text into runs
+    i.e "今天好熱！但是因為裡面有空調，所以還好" => ["今天好熱", "！", "但是因為裡面有空調", "，", "所以還好"]
+    """
+    return SEGMENT.findall(text)
+
+print(segment_words("今天好熱！但是因為裡面有空調，所以還好"))
+
+print(segment_words("This sentence is short."))
