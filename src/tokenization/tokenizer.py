849d8d11f7b9f78f2942838bda7a3000a3a8f248
Kyle Ng <kgng@usc.edu>
2025-09-01 22:25:53 -0700

    add encode function

--- src/tokenization/tokenizer.py
+++ src/tokenization/tokenizer.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
 import re
 import string
-from typing import Counter, Iterable, Mapping
+from typing import Iterable, Mapping
+from collections import Counter
 
 CJK_RANGE = r"\u4E00-\u9FFF"
 SEGMENT = re.compile(
@@ -89,7 +90,7 @@ class BPETokenizer:
     merge_ranks: dict[tuple[str, str], int] = field(default_factory=dict)
     token_to_id: dict[str, int] = field(default_factory=dict)
     id_to_token: dict[int, str] = field(default_factory=dict)
-    specials: list[str] = field(default_factory=lambda: [UNK])
+    specials: list[str] = field(default_factory=lambda: [UNK, EOW])
 
     def train(self,
               texts: Iterable[str],
@@ -100,16 +101,22 @@ class BPETokenizer:
         """
         # 1) Build vocab_counts from corpus
         vocab_counts: Counter[str] = Counter()
+        
+        # Add all symbols in original vocab to generalize tokenization
+        base_symbols: set[str] = set(self.specials)
         for text in texts:
             # Split text on punctuation/script change ["this", "is", "."]
             for w in segment_words(text):
                 # Split word to individual characters and <EOW>
                 # e.g. ["t", "h", "i", <EOW>] or ["你", “好”, <EOW>]
+                symbols = _word_to_symbols(w)
                 sym_seq = " ".join(_word_to_symbols(w))
                 vocab_counts[sym_seq] += 1
+                base_symbols.update(symbols)
 
         # 2) Merge high frequency co-occuring characters
         merges: list[tuple[str, str]] = []
+        merged_tokens :set[str] = set()
         for _ in range(num_merges):
             bigram = _get_bigram(vocab_counts)
             if not bigram:
@@ -122,6 +129,8 @@ class BPETokenizer:
             vocab_counts = _merge_token(best, vocab_counts)
             merges.append(best)
 
+            L,R = best
+            merged_tokens.add(L+R)
             print(merges)
 
         # Save merges and merge order for tokenization
@@ -133,16 +142,93 @@ class BPETokenizer:
         for word in vocab_counts.keys():
             token_set.update(word.split())
 
-        for special_token in self.specials + [EOW]:
-            token_set.add(special_token)
-
+        # Add all intermediate merge tokens and base tokens
+        token_set.update(base_symbols, merged_tokens)
+        
         # 4) Add token to id mapping
         token_list = sorted(token_set, key=lambda t: (len(t), t))
         self.token_to_id = {token: idx for idx, token in enumerate(token_list)}
         self.id_to_token = {idx: token for idx, token in enumerate(token_list)}
+
+    def _encode_word(self, word: str) -> list[str]:
+        """
+        Converts word from a string to a list of token strings w/ <EOW> token
+
+        Case: Priority Merge
+        e.g. word="the"
+        word => symbols = ["t", "h", "e", <EOW>] 
+        let merge_ranks = {("t","h"): 0, ("h", "e"): 1}
+        symbols = ["th", "e", <EOW>] # Note; "th" blocks the "he" merge
+
+        Case: Empty merge_ranks
+        e.g. word="the"
+        word => symbols = ["t", "h", "e", <EOW>] 
+        let merge_ranks = {}
+        symbols unchanged since no words available
+
+        Case: symbols unseen to merge
+        e.g. word="the"
+        word => symbols = ["t", "h", "e", <EOW>] 
+        let merge_ranks = {("w", "e"): 4}
+        symbols unchanged since unable to apply any merges
+        """
+        # 1) Map word to symbols
+        symbols = _word_to_symbols(word)
+
+        # 2) Check if learned merges are available
+        if not self.merge_ranks:
+            return symbols
+        # 3) Apply merges; make copy of `symbols`
+        # for safety in case _word_to_symbols returns an array
+        # not intended to be mutated
+        symbols = symbols[:]
+        while True:
+            # Generate all consecutive token pairs and look for tokens to merge
+            pairs = [(symbols[i], symbols[i+1]) for i in range(len(symbols)-1)]
+            ranked_merges = [(self.merge_ranks[p], p, i)
+                             for i, p in enumerate(pairs) if p in self.merge_ranks]
+
+            if not ranked_merges:
+                # No more possible merge matches, return the string as is and use fallbacks if needed
+                break
+
+            # find highest priority merge
+            _, (L, R), i = min(ranked_merges, key=lambda x: x[0])
+            symbols = symbols[:i] + [L+R] + symbols[2+i:]
+        return symbols
+
+    def encode(self, text: str) -> list[int]:
+        """
+        Convert continuous text to a list of corresponding token ids
+        e.g. "lower to tomorrow" => ["lo", "wer", "<EOW>", "to", "<EOW>", "to", "mor", "row", "<EOW>"] => [3, 4, 90, 20, 90, 20, 13, 41, 32, 90]
+        e.g. "今天很好" => ["今天 很 好", "<EOW>"] => [35, 90]
+        """
+        tokens = []
+        # Split continuous text into segments on whitespace/punctuation
+        for word in segment_words(text):
+            # Split word into subtokens
+            tokenized_word = self._encode_word(word)
+
+            # Add tokens one-by-one
+            for token in tokenized_word:
+                tokens.append(token)
+
+        # Turn each token into its corresponding ID
+        ids: list[int] = []
+        unk_id = self.token_to_id[UNK]
+        for token in tokens:
+            # <UNK> token fallback method
+            token_id = self.token_to_id.get(token, unk_id)
+
+            ids.append(token_id)
+
+        return ids
 
 
 tokenizer = BPETokenizer()
 tokenizer.train(
     texts=["low lower", "lowest 我的天", "我的好吃的蘋果"], num_merges=5
 )
+print(tokenizer._encode_word(word="lottery"))
+print(tokenizer.encode("low 我的 est"))
+breakpoint()
